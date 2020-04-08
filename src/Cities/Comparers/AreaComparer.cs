27ebf6de087aadd0acaa1eac083a6f6e3f3c07e4
Emma Troxell <troxellemma@gmail.com>
2020-04-08 18:43:08 -0500

    implemented population and area comparer

---
+++ src/Cities/Comparers/AreaComparer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cities.Comparers
+{
+    class AreaComparer : IComparer<City>
+    {
+        public int Compare(City x, City y)
+        {
+            if (x.Area > y.Area)
+            {
+                return -1;
+            }
+            if (x.Area == y.Area)
+            {
+                return 0;
+            }
+            return 1;
+        }
+    }
+}
---
+++ src/Cities/Comparers/PopulationComparer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cities.Comparers
+{
+    class PopulationComparer : IComparer<City>
+    {
+        public int Compare(City x, City y)
+        {
+            if (x.Population > y.Population)
+            {
+                return -1;
+            }
+            if (x.Population == y.Population)
+            {
+                return 0;
+            }
+            return 1;
+        }
+    }
+}
